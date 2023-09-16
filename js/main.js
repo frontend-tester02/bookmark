7a4eeb5da5052ba0f0b3255c7fa3087bfb2df075
frontend-tester02 <shokhrukhnuriddinov@gamil.com>
2023-09-17 00:08:00 +0900

    Refactor modifier classes into modifier object

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+const modifiers = {
+  tabItemActive:'tabs__item-active',
+  tabPanelActive:'tabpanels__item--active',
+  accordionItemOpen:'accordion__item--open'
+};
+
+
 const elsTabsItem = document.querySelectorAll('.tabs__item');
 const elsTabsPanel = document.querySelectorAll('.tabpanels__item');
 const elsTabLink = document.querySelectorAll('.js-tab-link');
@@ -8,20 +15,20 @@ const elsAccordionToggler = document.querySelectorAll('.accordion__item-toggler'
 
 function deactivateTabItems () {
   elsTabsItem.forEach(function (elTabsItem) {
-    elTabsItem.classList.remove('tabs__item-active');
+    elTabsItem.classList.remove(modifiers.tabItemActive);
   });
 
 }
 
 function deactivateTabPanels () {
   elsTabsPanel.forEach(function (elTabsPanel) {
-    elTabsPanel.classList.remove('tabpanels__item--active');
+    elTabsPanel.classList.remove(modifiers.tabPanelActive);
   });
 }
 
 function closeAccordionItems () {
   elsAccordionItem.forEach(function (elAccordionItem) {
-    elAccordionItem.classList.remove('accordion__item--open')
+    elAccordionItem.classList.remove(modifiers.accordionItemOpen)
 
   })
 }
@@ -39,7 +46,7 @@ elsTabLink.forEach(function (elTabLink) {
 
 
     // Add active class to current tabs__item
-    elTabLink.parentElement.classList.add('tabs__item-active');
+    elTabLink.parentElement.classList.add(modifiers.tabItemActive);
 
     // Remove active class from tabs__panel elements
     deactivateTabPanels();
@@ -47,7 +54,7 @@ elsTabLink.forEach(function (elTabLink) {
     // Show active tab panel
     // const elTargetPanel = document.querySelector(`#${elTabLink.href.split('#')[1]}`);
     const elTargetPanel = document.querySelector(elTabLink.dataset.tabTarget);
-    elTargetPanel.classList.add('tabpanels__item--active');
+    elTargetPanel.classList.add(modifiers.tabPanelActive);
 
 
   });
@@ -58,7 +65,7 @@ elsAccordionToggler.forEach(function (elAccordionItemToggler) {
   elAccordionItemToggler.addEventListener('click', function () {
     closeAccordionItems();
 
-    elAccordionItemToggler.closest('.accordion__item').classList.add('accordion__item--open');
+    elAccordionItemToggler.closest('.accordion__item').classList.add(modifiers.accordionItemOpen);
 
 
   });
